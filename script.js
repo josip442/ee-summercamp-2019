a1febbc6d54ed39c15ace867bb8f426edb14665d
josip442 <41233551+josip442@users.noreply.github.com>
2019-06-01 23:50:44 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,111 @@
+//MASA
+
+function poundsToKilograms(input) {
+    document.getElementById("pounds-kilograms").innerHTML = input / 2.2046;
+  }
+
+function poundsToOunces(input) {
+    document.getElementById("pounds-ounces").innerHTML = input * 16;
+}
+
+function poundsToGrams(input) {
+    document.getElementById("pounds-grams").innerHTML = input / 2.2046 * 1000;
+}
+
+function poundsToStones(input) {
+    document.getElementById("pounds-stones").innerHTML = input * 0.071429;
+}
+
+function kilogramsToPounds(input) {
+    document.getElementById("kilograms-pounds").innerHTML = input * 2.2046;
+}
+
+function kilogramsToOunces(input) {
+    document.getElementById("kilograms-ounces").innerHTML = input / 0.02834952;
+}
+
+function kilogramsToStones(input) {
+    document.getElementById("kilograms-stones").innerHTML = input * 0.15747;
+}
+
+function ouncesToPounds(input) {
+    document.getElementById("ounces-pounds").innerHTML = input / 16;
+}
+
+function ouncesToKilograms(input) {
+    document.getElementById("ounces-kilograms").innerHTML = input * 0.02834952;
+}
+
+function ouncesToGrams(input) {
+    document.getElementById("ounces-grams").innerHTML = input * 28.34952;
+}
+
+function ouncesToStones(input) {
+    document.getElementById("ounces-stones").innerHTML = input * 0.0044643;
+}
+
+function stonesToPounds(input) {
+    document.getElementById("stones-pounds").innerHTML = input * 14.000;
+}
+
+function stonesToKilograms(input) {
+    document.getElementById("stones-kilograms").innerHTML = input / 0.15747;
+}
+
+function stonesToOunces(input) {
+    document.getElementById("stones-ounces").innerHTML = input * 224;
+}
+
+function stonesToGrams(input) {
+    document.getElementById("stones-grams").innerHTML = input / 0.00015747;
+}
+
+// DULJINA
+
+function metersToMiles(input) {
+    document.getElementById("meters-miles").innerHTML = input * 0.000621;
+}
+
+function metersToInches(input) {
+    document.getElementById("meters-inches").innerHTML = input * 39.370079;
+}
+
+function metersToYards(input) {
+    document.getElementById("meters-yards").innerHTML = input * 1.093613;
+}
+
+function milesToMeters(input) {
+    document.getElementById("miles-meters").innerHTML = input / 0.000621;
+}
+
+function milesToInches(input) {
+    document.getElementById("miles-inches").innerHTML = input * 63360;
+}
+
+function milesToYards(input) {
+    document.getElementById("miles-yards").innerHTML = input * 1760;
+}
+//
+function inchesToMeters(input) {
+    document.getElementById("inches-meters").innerHTML = input * 0.0254;
+}
+
+function inchesToMiles(input) {
+    document.getElementById("inches-miles").innerHTML = input * 0.000015783;
+}
+
+function inchesToYards(input) {
+    document.getElementById("inches-yards").innerHTML = input * 0.027778;
+}
+
+function yardsToMeters(input) {
+    document.getElementById("yards-meters").innerHTML = input * 0.9144;
+}
+
+function yardsToInches(input) {
+    document.getElementById("yards-inches").innerHTML = input * 36;
+}
+
+function yardsToMiles(input) {
+    document.getElementById("yards-miles").innerHTML = input * 0.000568;
+}
